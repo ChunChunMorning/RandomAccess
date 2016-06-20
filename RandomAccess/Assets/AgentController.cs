f65db3ea65f8d1e98070b1e5e2c0399664d30e2e
asachun <chuchuchu.chunchun@gmail.com>
2016-06-20 16:15:46 +0900

    Create Students and Destination.

---
+++ RandomAccess/Assets/AgentController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class AgentController : MonoBehaviour
+{
+	NavMeshAgent m_NavMeshAgent;
+
+	void Awake ()
+	{
+		m_NavMeshAgent = GetComponent<NavMeshAgent> ();
+
+		var dst = GameObject.Find ("Destination");
+
+		m_NavMeshAgent.destination = dst.transform.position;
+	}
+}
