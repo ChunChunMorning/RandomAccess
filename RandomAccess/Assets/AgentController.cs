d352a2a7c3b25c1a268fbdcf2fd868388dfc08de
asachun <chuchuchu.chunchun@gmail.com>
2016-06-20 17:00:24 +0900

    Count population in Destination.

--- RandomAccess/Assets/AgentController.cs
+++ RandomAccess/Assets/AgentController.cs
@@ -3,13 +3,20 @@
 public class AgentController : MonoBehaviour
 {
 	NavMeshAgent m_NavMeshAgent;
+	DestinationConroller m_DestinationConroller;
 
 	void Awake ()
 	{
 		m_NavMeshAgent = GetComponent<NavMeshAgent> ();
+		m_DestinationConroller = FindObjectOfType<DestinationConroller> ();
 
 		var dst = GameObject.Find ("Destination");
 
 		m_NavMeshAgent.destination = dst.transform.position;
+	}
+
+	void Start ()
+	{
+		m_DestinationConroller.AddPopulation ();
 	}
 }
--- RandomAccess/Assets/DestinationConroller.cs
+++ RandomAccess/Assets/DestinationConroller.cs
@@ -1,20 +1,38 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class DestinationConroller : MonoBehaviour
 {
+	[SerializeField] int m_population;
+	List<DateTime> m_DateTimes;
+	List<int> m_Intervals;
+
 	void Awake ()
 	{
-
-	}
-
-	void Update ()
-	{
-
+		m_population = 0;
+		m_DateTimes = new List<DateTime> ();
+		m_Intervals = new List<int> ();
 	}
 
 	void OnTriggerEnter (Collider other)
 	{
+		var now = DateTime.Now;
+		m_DateTimes.Add (now);
+
+		var size = m_DateTimes.Count;
+		if (size > 0)
+		{
+			var ts = now - m_DateTimes [size - 1];
+			m_Intervals.Add (ts.Seconds);
+		}
+
+		--m_population;
 		Destroy (other.gameObject);
+	}
+
+	public void AddPopulation ()
+	{
+		++m_population;
 	}
 }
