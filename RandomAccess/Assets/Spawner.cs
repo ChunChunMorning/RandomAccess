f54a86a5ea34d611135bf2aad6c5209df412d238
asachun <chuchuchu.chunchun@gmail.com>
2016-06-20 19:06:01 +0900

    Add Spawner.

---
+++ RandomAccess/Assets/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+	[SerializeField] GameObject m_Students;
+
+	void Awake ()
+	{
+		const int size = 6;
+
+		for (int i = 0; i < size; ++i)
+		{
+			var pos = transform.position;
+
+			pos.x += 1.0f * Mathf.Cos (i / (float)size * 2.0f * Mathf.PI);
+			pos.z += 1.0f * Mathf.Sin (i / (float)size * 2.0f * Mathf.PI);
+
+			Instantiate (m_Students, pos, Quaternion.identity);
+		}
+	}
+}
