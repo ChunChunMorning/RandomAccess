72c000a64f89d580ce39459b8b40137cdbb8d254
asachun <chuchuchu.chunchun@gmail.com>
2016-06-20 16:30:00 +0900

    Create DestinationController.

---
+++ RandomAccess/Assets/DestinationConroller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestinationConroller : MonoBehaviour
+{
+	void Awake ()
+	{
+
+	}
+
+	void Update ()
+	{
+
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		Destroy (other.gameObject);
+	}
+}
